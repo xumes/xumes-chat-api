671132904403b9fdeae365b1b17a3185809ccd91
Reginaldo Santos <rmsantos@br.ibm.com>
2017-05-22 22:20:07 -0300

    working with CORS as a middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 var app = express();
 var server = require('http').Server(app)
@@ -18,6 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('mongo-host', '127.0.0.1')
